26ceb9b5a47a999a2ccb259a49f6a3af5c3ef80e
ArtemiyMalau <ya.fantic2012@yandex.ru>
2021-08-27 20:42:28 +0300

    Add example API methods

--- includes/api.php
+++ includes/api.php
@@ -6,5 +6,18 @@ use DigitalStars\SimpleAPI;
 $api = new SimpleAPI();
 
 switch ($api->module) {
-	
+	// Test requests
+	case "get_success":
+		$data = $api->params([]);
+
+		$api->answer["status"] = true;
+		$api->answer["response"] = ["payload": "success"];
+		break;
+
+	case "get_error":
+		$data = $api->params([]);
+
+		$api->answer["status"] = false;
+		$api->answer["error"] = "Example of error message";
+		break;
 }
