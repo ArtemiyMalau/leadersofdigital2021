27c8e14a1278c2ff16ff68d3575fc6768be77154
ArtemiyMalau <ya.fantic2012@yandex.ru>
2021-09-01 02:35:52 +0300

    Minor fix

--- includes/api.php
+++ includes/api.php
@@ -3,6 +3,9 @@ require_once "./functions.php";
 
 use DigitalStars\SimpleAPI;
 
+// localhost testing header
+header("Access-Control-Allow-Origin: *");
+
 $api = new SimpleAPI();
 
 switch ($api->module) {
@@ -11,13 +14,13 @@ switch ($api->module) {
 		$data = $api->params([]);
 
 		$api->answer["status"] = true;
-		$api->answer["response"] = ["payload" => "success"];
+		$api->answer["response"] = ["payload" => "success", "input_headers" => getallheaders(), "output_headers" => apache_response_headers()];
 		break;
 
 	case "get_error":
 		$data = $api->params([]);
 
 		$api->answer["status"] = false;
-		$api->answer["error"] = "Example of error message";
+		$api->answer["reason"] = "Example of error message";
 		break;
 }
